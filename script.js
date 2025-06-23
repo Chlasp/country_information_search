9172995c3163a6121de4d5b71c649ef9cd0922a0
Chlasp <davebalogun2015@gmail.com>
2025-06-23 15:45:11 -0400

    commit 2

--- script.js
+++ script.js
@@ -12,6 +12,12 @@ try {
 
     const capital = Countrydata[0]?.capital?.[0];
     if(!capital) throw new Error("Capital City not found.");
+    
+    // get news 
+   const newsResponse = await fetch(`https://gnews.io/api/v4/top-headlines?country=${country}&token=${newsApiKey}`);
+   const newsData = await newsResponse.json();
+   displayNews(newsData);
+   
 } catch (error){
     console.error(error);
     alert("Something went wrong. Check county name and try again.")
